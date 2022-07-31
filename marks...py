bba1d77eff8ba00fa13ba40ed02f7e5b04b7be73
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2022-07-31 16:08:28 +0530

    DAY -09

---
+++ marks...py
@@ -0,0 +1,22 @@
+print("-"*90)
+print("ISWARCHANDRA VIDYASAGAR ACADEMY  ")
+print("-"*90)
+print("                STUDENT REPORT  CARD         ")
+print("-"*90)
+name =input("ENTER A NAME OF STUDENT :")
+per =eval(input("ENTER A PERCENTAGES :"))
+print("-"*90)
+if per >= 90:
+    print("YOU GOT A A+ GRADE  :)")
+elif per >80 and per <90:
+    print("YOU GOT A A GRADE :)")
+elif per>60 and per<80:
+    print("YOU GOT A B+ GRADE ")
+elif per>50 and per<60:
+    print("YOU GOT A C GRADE ")
+elif per>=50:
+    print("YOU GOT A D GRADE")
+else:
+    print("YOU HAVE TO GIVE THE EXAM ")
+print("-"*90)
+print("DAY 09 COMPLETED :)")
