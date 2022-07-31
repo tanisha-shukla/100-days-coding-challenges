6f7c8d86829a6b7b9a6c9ecfe6f07c8c91c01475
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2022-07-31 12:51:58 +0530

    DAY -02

---
+++ rectangles.py
@@ -0,0 +1,11 @@
+print("-"*100)
+length =eval(input("ENTER A LENGTH OF RECTANGLE  :"))
+breath=eval(input("ENTER A BREADTH OF RECTANGLE:"))
+print("-"*100)
+area =length*breath
+perimeter =2*(length+breath)
+print("AREA OF RECTANGLE IS  :", area,"sq cm")
+print("PERIMETER OF RECTANGLR IS :", perimeter,"cm")
+print("-"*100)
+print(" DAY - 02 COMPLETED :)  ")
+print("-"*100)
