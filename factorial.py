90e608ddbcdfd067cab05271e4f06c70463b6763
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2022-07-31 16:25:20 +0530

    DAY -12

---
+++ factorial.py
@@ -0,0 +1,10 @@
+print("~"*90)
+print("                 FACTORIAL OF NUMBER                                             ")
+print("~"*90)
+num =int(input("ENTER A NUMBER :"))
+S =1
+for i in range (num,0,-1):
+    S =S*i
+print(" FACTORIAL OF ", num , "IS : " ,S)
+print("~"*90)
+print("DAY 12 COMPLETED :)")
