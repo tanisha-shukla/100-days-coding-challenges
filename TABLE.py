b1f77856f0aa27f5d2f6b35924a8c9c36760f8a5
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2022-07-31 16:20:51 +0530

    DAY -11

---
+++ TABLE.py
@@ -0,0 +1,12 @@
+print("~"*90)
+n= int(input("ENTER A MULTIPLICATION OF NUMBER :"))
+print("~"*90)
+print("      MULTIPLICATION  OF    " ,      n    , "ARE    : ")
+print(" "*90)
+for i in range(1,11):
+    res =n*i
+
+    print(n, "X" , i ,"=" , res )
+print("~"*90)
+print("DAY - 11 COMPLETED :)")
+print("~"*90)
