b8d6843d08b3df47c5adb4024bee4c43471b425d
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2022-08-26 00:21:26 +0530

    DAY - 27

---
+++ pyramid.py
@@ -0,0 +1,12 @@
+print("-"*90)
+print("       ITERATIONS OF FOR LOOP    ")
+print("-"*90)
+
+n =int(input("ENTER THE  NUMBER  OF ROWS :"))
+print("-"*90)
+for i in range(n , 0,-1):
+    for p in range(1 ,i+1 ):
+        print(chr(64+p), end = '')
+    print()
+print("-"*90)
+print(" DAY - 27 COMPLETED :)")
