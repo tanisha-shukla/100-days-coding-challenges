1140a45819efad7538522821fa4cfa0a3ad335aa
Tanisha_Shukla <110094840+tanisha-shukla@users.noreply.github.com>
2024-12-26 18:39:03 +0530

    DAY - 94

---
+++ Create a Bar Graph using Matplotlib Library.py
@@ -0,0 +1,15 @@
+print("Create a Bar Graph using Matplotlib Library ")
+
+import matplotlib.pyplot as pt
+
+a=['Python','Math','ECE','Phy','Eng','HTML','I.P','Mech']
+b=[99,76,67,89,85,79,81,90]
+pt.bar(a,b,color=('salmon','green','olive','teal','tan','black','grey','pink'))
+pt.title("Student Report ")
+pt.xlabel("Subjects ")
+pt.ylabel("Marks")
+pt.show()
+
+
+
+print("DAY - 94 ")
